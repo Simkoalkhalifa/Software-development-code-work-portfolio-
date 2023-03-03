0c5ab3bcf0e419ec0c392736d902bc5b47788997
simko <simko.al.khalifa@gmail.com>
2023-03-03 13:20:26 +0000

    initial version

---
+++ src/Dowhileloop.java
@@ -0,0 +1,14 @@
+public class Dowhileloop {
+    public static void main(String[] args) {
+
+
+        int x = 0;
+
+        do {
+            System.out.print
+                    ("Execute " + x);
+            x++;
+            System.out.print("\n");
+        } while (x < 20);
+    }
+}
---
+++ src/Loops.java
@@ -0,0 +1,8 @@
+// for loop
+public class Loops {
+    public static void main(String[] args) {
+        for (int counter = 1; counter <= 10; counter++) {
+            System.out.println(counter + "- Kick the enemy!");
+        }
+    }
+}
